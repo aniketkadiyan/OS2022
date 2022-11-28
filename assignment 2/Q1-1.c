5401b534fef18fc91c2010614848fe1e59dc3c21
aniketkadiyan <42398208+aniketkadiyan@users.noreply.github.com>
2022-11-28 11:18:53 +0530

    Update Q1-1.c

--- assignment 2/Q1-1.c
+++ assignment 2/Q1-1.c
@@ -2,13 +2,26 @@
 #include <pthread.h>
 #include <sched.h>
 #include <stdio.h>
-#include "check.h"
+# include <time.h>
+#include <unistd.h>
+
+#define BILLION  1000000000L;
+
+int i=2;
 
 void *threadfunc(void *parm)
 {
+	struct timespec start, stop;
+double accum;
   printf("Inside secondary thread\n");
-  sleep(5);  
-  return NULL;
+  int x=0;
+  int y = 4294967295
+  while(x<y){
+	  x=x+1;
+  }
+accum = ( stop.tv_sec - start.tv_sec )+ (double)( stop.tv_nsec - start.tv_nsec ) / (double)BILLION;
+printf( "%lf\n", accum );
+  pthread_exit(&i)
 }
 
 int main(int argc, char **argv)
@@ -20,22 +33,36 @@ int main(int argc, char **argv)
   struct sched_param param;
   int policy;
 
-  printf("Enter Testcase - %s\n", argv[0]);
-
-
   rc = pthread_attr_init(&tattr);
   rc = pthread_attr_getschedparam(&tattr, &param);
   param.sched_priority = t_priority;
   rc = pthread_attr_setschedpolicy(&tattr , SCHED_RR);
   rc = pthread_attr_setschedparam(&tattr, &param);
-  rc = pthread_create(&thread, &tattr, threadfunc, NULL);
+  rc = pthread_create(&thread, &tattr, threadfunc, &param);
   rc = pthread_getschedparam(thread, &policy, &param);
+	int *ptr;
+pthread_join(thread , (void**)&ptr)
 
-  printf("The thread scheduling parameters indicate:\n"
-         "policy = %d\n", policy);
-  printf("priority = %d\n",
-         param.sched_priority);
 
+ rc = pthread_attr_init(&tattr);
+  rc = pthread_attr_getschedparam(&tattr, &param);
+  param.sched_priority = t_priority;
+  rc = pthread_attr_setschedpolicy(&tattr , SCHED_FIFO);
+  rc = pthread_attr_setschedparam(&tattr, &param);
+  rc = pthread_create(&thread, &tattr, threadfunc, &param);
+  rc = pthread_getschedparam(thread, &policy, &param);
+	int *ptr;
+pthread_join(thread , (void**)&ptr)
+
+ rc = pthread_attr_init(&tattr);
+  rc = pthread_attr_getschedparam(&tattr, &param);
+  param.sched_priority = t_priority;
+  rc = pthread_attr_setschedpolicy(&tattr , SCHED_OTHER);
+  rc = pthread_attr_setschedparam(&tattr, &param);
+  rc = pthread_create(&thread, &tattr, threadfunc, &param);
+  rc = pthread_getschedparam(thread, &policy, &param);
+	int *ptr;
+pthread_join(thread , (void**)&ptr)
   printf("Main completed\n");
   return 0;
 }
